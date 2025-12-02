b07dd8f4e17f45897ccb9578138fb9bf7b175d39
KhassandraJayme <khassandrajayme@gmail.com>
2025-12-02 09:14:25 +0800

    Update summary.py

--- plotease/summary.py
+++ plotease/summary.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
-from .visualization import VisualizationBase 
-from typing import Optional, List, Dict 
+from .visualization import VisualizationBase
+from typing import Optional, List, Dict
 
 class SummaryGenerator(VisualizationBase):
     """
